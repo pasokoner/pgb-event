d0ccdcbecfd1ec117e1e2e229598ea5edb75e0dd
John Carlo Asilo <jcarloasilo@gmail.com>
2023-11-09 09:47:56 +0800

    ui improvement

--- src/app/(authed)/offices/page.tsx
+++ src/app/(authed)/offices/page.tsx
@@ -20,7 +20,7 @@ export default async function OfficesPage() {
     <div>
       <div className="mb-4 flex flex-wrap justify-between gap-4 sm:mb-6">
         <div>
-          <h1 className="text-3xl font-bold text-gray-700">Offices</h1>
+          <h1 className="text-3xl font-bold text-gray-700">OFFICES</h1>
           <p className="text-gray-500">Manage your offices</p>
         </div>
 
